f39d6ccaac4f345b42878f1f5b2eecef747229a4
Lesko <polensek.matevz@gmail.com>
2024-06-02 21:24:38 +0200

    Changed db again.

--- gms_app/models.py
+++ gms_app/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# def get_default_plant():
-#     return PlantPreset.objects.get(name="default")
-
 
 class Garden(models.Model):
     name = models.CharField(max_length=50)
@@ -13,8 +10,17 @@ class Garden(models.Model):
 
 
 class GardenBed(models.Model):
-    garden = models.ForeignKey(Garden, on_delete=models.CASCADE)
+    garden = models.ForeignKey(Garden, blank=True, on_delete=models.DO_NOTHING, null=True)
     name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Plant(models.Model):
+    garden_bed = models.ManyToManyField(GardenBed, blank=True, null=True)
+    name = models.CharField(max_length=50, unique=True)
     description = models.TextField(blank=True)
 
     def __str__(self):
@@ -24,13 +30,6 @@ class GardenBed(models.Model):
 class PlantPreset(models.Model):
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField(blank=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Plant(PlantPreset):
-    garden_bed = models.ManyToManyField(GardenBed, blank=True)
 
     def __str__(self):
         return self.name
