a2575f37cbf6fb61f1e8e51696c3630988328fd1
Lesko <polensek.matevz@gmail.com>
2024-06-03 20:00:07 +0200

    Added views and ... database ... again.

--- gms_app/models.py
+++ gms_app/models.py
@@ -13,15 +13,17 @@ class GardenBed(models.Model):
     garden = models.ForeignKey(Garden, blank=True, on_delete=models.DO_NOTHING, null=True)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+    location = models.JSONField(default=dict)
 
     def __str__(self):
         return self.name
 
 
 class Plant(models.Model):
-    garden_bed = models.ManyToManyField(GardenBed, blank=True, null=True)
+    garden_bed = models.ManyToManyField(GardenBed, blank=True)
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField(blank=True)
+    location = models.JSONField(default=dict)
 
     def __str__(self):
         return self.name
---
+++ gms_app/urls.py
@@ -0,0 +1,14 @@
+# Documentation is like sex.
+# When it's good, it's very good.
+# When it's bad, it's better than nothing.
+# When it lies to you, it may be a while before you realize something's wrong.
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.GardenHomeView.as_view(), name="home"),
+    path("garden/", views.GardenListView.as_view(), name="garden_list"),
+    path("garden/<int:pk>/", views.GardenDetailView.as_view(), name="garden_detail"),
+    # path("garden/addplant/", views.AddPlantView.as_view(), name="add_plant"),
+]
