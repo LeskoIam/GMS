9d850a72bea0334a4b021c3fa283a5e71c1265f2
Lesko <polensek.matevz@gmail.com>
2024-06-05 21:24:08 +0200

    Fixed verbose names for PlantPreset and improved Planting string representation.

--- gms_app/admin.py
+++ gms_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Garden, GardenBed, Plant, PlantPreset
+from .models import Garden, GardenBed, Plant, Planting, PlantPreset
 
 admin.site.register(Garden)
 admin.site.register(GardenBed)
 admin.site.register(Plant)
 admin.site.register(PlantPreset)
+admin.site.register(Planting)
