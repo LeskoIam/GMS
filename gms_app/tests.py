a2cef55e447403bc96333b2ccf159f655740f584
John Baldwin <jlbaldwin@gmail.com>
2024-06-04 22:14:22 -0500

    Adds new Planting join model
    This commit creates a new join models, "Planting" that allows multiple
    references of the same Plant to be in a garden bed (like planting a row
    of radishes). It moves Plant.location to be Planting.location since a
    plant "type" can have none, one, or many locations.
    
    - Also adds metadata to the models and model fields
    - Updates tests

--- gms_app/tests.py
+++ gms_app/tests.py
@@ -3,7 +3,7 @@ import logging
 import pytest
 from django.db.utils import IntegrityError
 
-from gms_app import models
+from gms_app.models import Garden, GardenBed, Plant, Planting
 
 log = logging.getLogger(__name__)
 
@@ -12,39 +12,52 @@ log = logging.getLogger(__name__)
 def _prepare_db(django_db_setup, django_db_blocker):
     # Set up non-modified objects used by all test methods
     with django_db_blocker.unblock():
-        garden = models.Garden.objects.create(name="Test Garden", description="Test Garden Description")
-        garden_bed = models.GardenBed.objects.create(
-            garden=garden, name="Test Garden Bed", description="Test Garden Bed Description 1"
+        garden = Garden.objects.create(
+            name="Test Garden",
+            description="Test Garden Description",
+        )
+        garden_bed = GardenBed.objects.create(
+            garden=garden,
+            name="Test Garden Bed",
+            description="Test Garden Bed Description 1",
         )
         log.info("Garden '%s' - garden bed '%s'", garden.name, garden_bed.name)
         for plant_i in range(3):
-            p = models.Plant(name=f"Test Plant {plant_i}", description=f"Test Plant Description{plant_i}")
-            p.save()
-            p.garden_bed.add(garden_bed)
-            log.info("\tAdded plant '%s'", p.name)
+            plant = Plant.objects.create(
+                name=f"Test Plant {plant_i}",
+                description=f"Test Plant Description{plant_i}",
+            )
+            planting = Planting.objects.create(
+                plant=plant,
+                garden_bed=garden_bed,
+                location={"x": plant_i, "y": 0},
+            )
+            log.info("\tAdded plant '%s'", plant.name)
 
 
 @pytest.mark.django_db()
 def test_numer_of_garden_beds():
     """Number of garden beds for garden is correct."""
-    garden = models.Garden.objects.get(name="Test Garden")
-    beds = models.GardenBed.objects.filter(garden=garden)
+    garden = Garden.objects.get(name="Test Garden")
+    beds = GardenBed.objects.filter(garden=garden)
 
     assert len(beds) == 1
 
 
 @pytest.mark.django_db()
-def test_number_of_plants():
+def test_number_of_plant_in_first_garden_bed():
     """Number of plants for garden bed is correct."""
-    garden = models.Garden.objects.get(name="Test Garden")
-    beds = models.GardenBed.objects.filter(garden=garden)
-    plants = models.Plant.objects.filter(garden_bed=beds[0])
-
+    garden = Garden.objects.get(name="Test Garden")
+    garden_bed = GardenBed.objects.filter(garden=garden).first()
+    plants = garden_bed.plants.all()
     assert len(plants) == 3
+    # or simply
+    # assert garden_bed.plants.count() == 3
 
 
 @pytest.mark.django_db()
-def test_allow_same_plant_on_bed():
+def test_allow_duplicate_plants_on_bed():
+
     """We need to be able to have multiples of plants on the same garden bed.
 
     Given garden with garden beds
@@ -52,11 +65,12 @@ def test_allow_same_plant_on_bed():
     When we add another "Test Plant 1" plant on the same garden bed
     Then it should add the plant to the garden bed
     """
-    garden = models.Garden.objects.get(name="Test Garden")
-    beds = models.GardenBed.objects.filter(garden=garden)
-    plant = models.Plant(name="Test Plant 1", description="Test Garden Bed Description")
-    try:
-        plant.save()  # <-- This will raise IntegrityError
-        plant.garden_bed.add(beds)
-    except IntegrityError:
-        pytest.fail("Couldn't save the plant. Only one unique plant per garden STILL allowed!", pytrace=True)
+    garden = Garden.objects.get(name="Test Garden")
+    plant = Plant.objects.first()
+    # Brittle. Assumes knowledge of the locations in the model factory fixture
+    location = {"x": 3, "y": 0}
+
+    for bed in GardenBed.objects.filter(garden=garden):
+        planting = bed.add_plant(plant, location)
+        assert planting.plant == plant
+        assert planting.location == location
