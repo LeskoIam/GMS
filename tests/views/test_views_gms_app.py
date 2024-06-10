7d76ae2129a39d754ddebbff9e53d63abc7bb7a0
Lesko <polensek.matevz@gmail.com>
2024-06-10 19:27:09 +0200

    Renamed test function to singular.

--- tests/views/test_views_gms_app.py
+++ tests/views/test_views_gms_app.py
@@ -14,7 +14,7 @@ log = logging.getLogger(__name__)
 
 @pytest.mark.parametrize(("viewname", "kwargs_"), [("home", {}), ("garden_list", {}), ("garden_detail", {"pk": 1})])
 @pytest.mark.django_db()
-def test_views_loads(client, viewname, kwargs_):
+def test_view_loads(client, viewname, kwargs_):
     """We want to know if basic views load without error.
 
     :param viewname: name of the view to test
