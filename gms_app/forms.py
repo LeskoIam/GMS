7dac8138f0ca0ffe8436cca84a3a379fe28c02c0
Lesko <polensek.matevz@gmail.com>
2024-06-12 20:49:53 +0200

    [#28] Adding form for linking Plants to GardenBeds.

--- gms_app/forms.py
+++ gms_app/forms.py
@@ -3,16 +3,12 @@
 # When it's bad, it's better than nothing.
 # When it lies to you, it may be a while before you realize something's wrong.
 
-from django.forms import BooleanField, CheckboxInput, ModelForm, Textarea, TextInput
+from django.forms import BooleanField, CheckboxInput, ChoiceField, ModelForm, Select, Textarea, TextInput
 
-from .models import Plant
+from .models import Plant, Planting
 
 
 class AddPlantForm(ModelForm):
-    # save_plant_preset = BooleanField(widget=CheckboxInput(
-    #     attrs={'id': 'flexCheckDefault', "class": "form-check-input", "type": "checkbox", "checked": ""}),
-    #     label="Save Plant Preset"
-    # )
     add_plant_to_planting = BooleanField(
         widget=CheckboxInput(attrs={"id": "flexCheckDefault", "class": "form-check-input", "type": "checkbox"}),
         label="Add Plant to Planting",
@@ -35,3 +31,14 @@ class AddPlantForm(ModelForm):
                 }
             ),
         }
+
+
+class AddPlantToGardenBedForm(ModelForm):
+    class Meta:
+        model = Planting
+        fields = ["plant", "garden_bed", "location"]
+        widgets = {
+            "plant": Select(attrs={"class": "form-select", "size": "7", "aria-label": "size 7 select example"}),
+            "garden_bed": Select(attrs={"class": "form-select", "size": "4", "aria-label": "size 4 select example"}),
+        }
+        # attrs={"class": "form-select", "size": "7", "multiple": None, "aria-label": "multiple size 7 select example"}
