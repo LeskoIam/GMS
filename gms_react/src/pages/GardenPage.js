fd33a77b88d9bdb40a15e84633723648c6d5d24a
Lesko <polensek.matevz@gmail.com>
2024-07-04 11:56:25 +0200

    Adding components and improvements for Garden/s views.

--- gms_react/src/pages/GardenPage.js
+++ gms_react/src/pages/GardenPage.js
@@ -1,11 +1,13 @@
 import Body from '../components/Body';
-import Gardens from "../components/Gardens";
+import {useParams} from "react-router-dom";
 
 export default function GardenPage() {
+  const { key } = useParams();
+
   return (
     <Body sidebar>
-      <h1>Gardens</h1>
-      <Gardens />
+      <h1>{key}</h1>
+      <p>TODO</p>
     </Body>
   );
 }
